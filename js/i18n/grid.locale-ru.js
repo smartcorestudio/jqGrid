0716b885a2485a8afa6d27e967101495d05643d4
smartcorestudio <studio@smartcore.ru>
2014-04-12 12:20:19 +0400

    operandTitle and resetTitle Russian translation

--- js/i18n/grid.locale-ru.js
+++ js/i18n/grid.locale-ru.js
@@ -21,8 +21,8 @@ $.extend($.jgrid,{
 		Reset: "Сброс",
 		odata: [{ oper:'eq', text:"равно"},{ oper:'ne', text:"не равно"},{ oper:'lt', text:"меньше"},{ oper:'le', text:"меньше или равно"},{ oper:'gt', text:"больше"},{ oper:'ge', text:"больше или равно"},{ oper:'bw', text:"начинается с"},{ oper:'bn', text:"не начинается с"},{ oper:'in', text:"находится в"},{ oper:'ni', text:"не находится в"},{ oper:'ew', text:"заканчивается на"},{ oper:'en', text:"не заканчивается на"},{ oper:'cn', text:"содержит"},{ oper:'nc', text:"не содержит"},{ oper:'nu', text:"равно NULL"},{ oper:'nn', text:"не равно NULL"}],
 		groupOps: [	{ op: "AND", text: "все" }, { op: "OR", text: "любой" }],
-		operandTitle : "Click to select search operation.",
-		resetTitle : "Reset Search Value"
+		operandTitle : "Кликните для выбора операции сравнения.",
+		resetTitle : "Сбросить фильтр"
 	},
 	edit : {
 		addCaption: "Добавить запись",
